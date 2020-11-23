d3b51ae17885393bc529c734664533f0ab23a800
Juslin Jukka <jusju@haaga-helia.fi>
2020-11-23 12:23:31 +0200

    first commit

---
+++ src/main/java/com/gallery/gallery/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.gallery.gallery;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+    private UserDetailsService userDetailsService;
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests().antMatchers("/css/**").permitAll() // Enable css when logged out
+        .and()
+        .authorizeRequests().antMatchers("/signup", "/saveuser").permitAll()
+        .and()
+          .authorizeRequests().anyRequest().authenticated()
+          .and()
+      .formLogin()
+          .loginPage("/login")
+          .defaultSuccessUrl("/index", true)
+          .permitAll()
+          .and()
+      .logout()
+          .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+}
